8ce8d41d9b70853dfa68fd603fe3207c03951255
Daijiro Wachi <daijiro.wachi@gmail.com>
2022-06-27 14:33:21 +0900

    support version

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # vulcan-fhir-resource-content-extractor
 Extract FHIR Resource content as csv header to support mapping of FHIR to something elsex.
 
+## Folders and files
+Place a FHIR's version folder and resource definitions in xml into the folder
+```
+FHIR
+└── R4B
+    ├── observation.xml
+    └── patient.xml
+```
+
+`npm run build` generates the list of resource content in csv as `OUT/<version>/<resource name>.xml`.
+```
+OUT
+└── R4B
+    ├── observation.csv
+    └── patient.csv
+```
+
 ## Getting started
 1. Install dependencies.
 
@@ -15,5 +32,5 @@ $ npm run build
 
 3. Check outputs
 ```bash
-$ open out
+$ open OUT
 ```
--- scripts/parse.js
+++ scripts/parse.js
@@ -2,28 +2,31 @@ const fs = require('fs')
 const path = require('path')
 const { XMLParser } = require('fast-xml-parser')
 const { Parser, transforms: { flatten } } = require('json2csv');
-const RESOURCE_PATH = path.join(__dirname, '../R4B/')
-const OUT_PATH = path.join(__dirname, '../out/')
-const list = fs.readdirSync(RESOURCE_PATH)
+const RESOURCE_PATH = path.join(__dirname, '../FHIR/')
+const OUT_PATH = path.join(__dirname, '../OUT/')
+const versions = fs.readdirSync(RESOURCE_PATH)
 
-list.forEach((item) => {
-  const json2csv = new Parser({
-    transforms: [flatten({
-      objects: true,
-      arrays: true
-    })]
-  })
-  const xml2json = new XMLParser()
-  const xml = fs.readFileSync(path.join(RESOURCE_PATH, item), 'utf8')
-  const json = xml2json.parse(xml)
-  const csv = json2csv.parse(json)
-  const parsedCsv = csv.split(',')
-    .map((item) => {
-      return item.replace(/"/g, '').replace('[x]', '').replace('#', '')
+versions.forEach((version) => {
+  const resources = fs.readdirSync(path.join(RESOURCE_PATH, version))
+  resources.forEach((resource) => {
+    const json2csv = new Parser({
+      transforms: [flatten({
+        objects: true,
+        arrays: true
+      })]
     })
-    .filter((item) => {
-      return item !== '' && item !== 'doco'
-    })
-    .join(',')
-  fs.writeFileSync(path.join(OUT_PATH, item.replace('xml', 'csv')), parsedCsv);
+    const xml2json = new XMLParser()
+    const xml = fs.readFileSync(path.join(RESOURCE_PATH, version, resource), 'utf8')
+    const json = xml2json.parse(xml)
+    const csv = json2csv.parse(json)
+    const parsedCsv = csv.split(',')
+      .map((item) => {
+        return item.replace(/"/g, '').replace('[x]', '').replace('#', '')
+      })
+      .filter((item) => {
+        return item !== '' && item !== 'doco'
+      })
+      .join(',')
+    fs.writeFileSync(path.join(OUT_PATH, version, resource.replace('xml', 'csv')), parsedCsv);
+  });
 })
